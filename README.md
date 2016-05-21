9fb5d25c7ef4202b78a385ef03d5a3ba11b05b47
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2016-05-21 15:17:42 +0200

    Add line graph

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-'''
+## Install
+```
 bower install dc.js
-'''
+```
--- scripts/tutorial.js
+++ scripts/tutorial.js
@@ -9,24 +9,50 @@ function print_filter(filter){
 }
 
 var data = [
-  {date: "2011-11-14T16:17:54Z", quantity: 2, total: 190, tip: 100, type: "tab"},
-  {date: "2011-11-14T16:20:19Z", quantity: 2, total: 190, tip: 100, type: "tab"},
-  {date: "2011-11-14T16:28:54Z", quantity: 1, total: 300, tip: 200, type: "visa"},
-  {date: "2011-11-14T16:30:43Z", quantity: 2, total: 90, tip: 0, type: "tab"},
-  {date: "2011-11-14T16:48:46Z", quantity: 2, total: 90, tip: 0, type: "tab"},
-  {date: "2011-11-14T16:53:41Z", quantity: 2, total: 90, tip: 0, type: "tab"},
-  {date: "2011-11-14T16:54:06Z", quantity: 1, total: 100, tip: 0, type: "cash"},
-  {date: "2011-11-14T16:58:03Z", quantity: 2, total: 90, tip: 0, type: "tab"},
-  {date: "2011-11-14T17:07:21Z", quantity: 2, total: 90, tip: 0, type: "tab"},
-  {date: "2011-11-14T17:22:59Z", quantity: 2, total: 90, tip: 0, type: "tab"},
-  {date: "2011-11-14T17:25:45Z", quantity: 2, total: 200, tip: 0, type: "cash"},
-  {date: "2011-11-14T17:29:52Z", quantity: 1, total: 200, tip: 100, type: "visa"}
-];
+		{date: "12/27/2012", http_404: 2, http_200: 190, http_302: 100},
+		{date: "12/28/2012", http_404: 2, http_200: 10, http_302: 100},
+		{date: "12/29/2012", http_404: 1, http_200: 300, http_302: 200},
+		{date: "12/30/2012", http_404: 2, http_200: 90, http_302: 0},
+		{date: "12/31/2012", http_404: 2, http_200: 90, http_302: 0},
+		{date: "01/01/2013", http_404: 2, http_200: 90, http_302: 0},
+		{date: "01/02/2013", http_404: 1, http_200: 10, http_302: 1},
+		{date: "01/03/2013", http_404: 2, http_200: 90, http_302: 0},
+		{date: "01/04/2013", http_404: 2, http_200: 90, http_302: 0},
+		{date: "01/05/2013", http_404: 2, http_200: 90, http_302: 0},
+		{date: "01/06/2013", http_404: 2, http_200: 200, http_302: 1},
+		{date: "01/07/2013", http_404: 1, http_200: 200, http_302: 100}
+		];
 
 var ndx = crossfilter(data);
 
-var totalDim = ndx.dimension(function(d) { return d.total; });
+console.log(data)
 
-var total_90 = totalDim.filter(90);
+var parseDate = d3.time.format("%m/%d/%Y").parse;
 
-print_filter("total_90");
+data.forEach(function(d) {
+	d.date = parseDate(d.date);
+	d.total= d.http_404+d.http_200+d.http_302;
+});
+
+print_filter("data");
+
+var dateDim = ndx.dimension(function(d) {return d.date;});
+var hits = dateDim.group().reduceSum(function(d) {return d.total;});
+
+console.log(hits.size());
+
+var minDate = dateDim.bottom(1)[0].date;
+var maxDate = dateDim.top(1)[0].date;
+console.log(dateDim.bottom(1)[0].date)
+
+var hitslineChart  = dc.lineChart("#chart-line-hitsperday");
+
+hitslineChart
+	.width(500).height(200)
+	.dimension(dateDim)
+	.group(hits)
+	.x(d3.time.scale().domain([minDate,maxDate]));
+
+console.log(hitslineChart);
+
+dc.renderAll();
